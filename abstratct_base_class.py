e1fcd0d7882e4032e79377f71386467ca04e988b
vaibhav <verma88lko@gmail.com>
2020-09-17 14:49:31 +0530

    Version 2

--- abstratct_base_class.py
+++ abstratct_base_class.py
@@ -14,10 +14,17 @@ class rectangle(shape):
         return (self.length * self.breath)
 
 rect1 = rectangle()
-print(rect1.print_area())
+print(f"AREA = {rect1.print_area()}")
 
 class square(shape):
     def __init__(self):
         self.side = 4
 
+    def print_area(self):
+        return (self.side* self.side)
+
 sq1 = square()
+
+# as we can see that class shape is throwing an error and asking us to define print_area in class square.
+# shape class is giving orders to square class to implement print_arae
+# once we define print_arae in square then it won't throw any error
